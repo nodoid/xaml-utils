1dd560fce549db812b15b484696f3cd88ad788e4
nodoid <paul@all-the-johnsons.co.uk>
2014-09-13 04:37:01 +0100

    moved the holder class to a new file

---
+++ xib2xaml/xib2xaml/Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace xib2xaml
+{
+    class UIObject
+    {
+        public string UIName { get; set; }
+
+        public string UIElement { get; set; }
+
+        public string Text { get; set; }
+
+        public string FontSize { get; set; }
+
+        public string UIWidth { get; set; }
+
+        public string UIHeight { get; set; }
+
+        public string UIXPos { get; set; }
+
+        public string UIYPos { get; set; }
+
+        public string BackgroundColor { get; set; }
+
+        public string TextColor { get; set; }
+
+        public string OtherDetails { get; set; }
+
+        public string ColorA { get; set; }
+
+        public string ColorB { get; set; }
+
+        public string ColorG { get; set; }
+
+        public string ColorR { get; set; }
+
+        public string ColorW { get; set; }
+
+        public string TextHAlign { get; set; }
+
+        public string TextVAlign { get; set; }
+    }
+}
+
